21b222d888dd276ca3898b86bd4080aff5ad3eff
letangileri <let.angileri@gmail.com>
2025-09-03 18:36:23 +0200

    update

--- server.js
+++ server.js
@@ -13,3 +13,41 @@ app.get("/", (req, res) =>{
     res.send("Server del mio blog");
 })
 
+app.get("/bacheca", (req,res)=>{
+    const post = [
+        {
+            titolo:"Ciambellone",
+            contenuto:"ciambella",
+            immagine:"images/ciambellone.jpeg",
+            tag:"sono un tag ciambellone"
+        },
+        {
+            titolo:"Cracker_barbabietola",
+            contenuto:"Cracker",
+            immagine:"images/cracker_barbabietola.jpeg",
+            tag:"sono un tag cracker_barbabietola"
+        },
+        {
+            titolo:"Pane fritto dolce",
+            contenuto:"Pane fritto",
+            immagine:"images/pane_fritto_dolce.jpeg",
+            tag:"sono un tag pane fritto"
+        },
+        {
+            titolo:"Pasta barbabietola",
+            contenuto:"pasta",
+            immagine:"images/pasta_barbarbietola.jpeg",
+            tag:"sono un tag barbabietola"
+        },
+        {
+            titolo:"Torta paesana",
+            contenuto:"torta",
+            immagine:"images/torta_paesana.jpeg",
+            tag:"sono un tag torta paesana"
+        },
+    ]
+    
+    res.json(post);
+})
+
+
